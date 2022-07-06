2af5798906d529a6f7c7a8ca3d2bad6e4b724302
brovolia <40294728+brovolia@users.noreply.github.com>
2022-07-06 17:24:58 +0200

    Add files via upload

---
+++ install_required_packages.R
@@ -0,0 +1,9 @@
+#Install the necessary packages
+BiocManager::install("shiny")
+BiocManager::install("WGCNA")
+BiocManager::install("ggplot2")
+
+#load the libraries
+library(shiny)
+library(WGCNA)
+library(ggplot2)
